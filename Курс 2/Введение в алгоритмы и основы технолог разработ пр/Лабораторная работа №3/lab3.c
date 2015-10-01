c6ee1bc7db0f8a325df1c6e1f136801db2e81551
Vladislav Tsisyk <etawinsky@gmail.com>
2015-10-01 19:48:51 +0600

    сделал третью лабу по алгоритмам

--- Курс 2/Введение в алгоритмы и основы технолог разработ пр/Лабораторная работа №3/lab3.c
+++ Курс 2/Введение в алгоритмы и основы технолог разработ пр/Лабораторная работа №3/lab3.c
@@ -3,18 +3,51 @@
 
 #include <stdio.h>
 #include "list.h"
-
+#define LINELENGTH 1024
 int main()
 {
 
+	FILE *fp;
+    char line[LINELENGTH];
+    char *p;
+	int c, n;
 	struct List *test = NULL;
+	struct List *answer = NULL;
 	test = list_init();
-	list_insert(test, 10);
-	printf("xbckj %d\n", test->tail->number);
-	list_insert(test, 16);
-	printf("xbckj %d\n", test->tail->number);
-	printf("xbckj %d\n", test->head->number);
-	test->head = test->head->next;
-	printf("xbckj %d\n", test->head->number);
+    printf("введите имя файла:");
+    scanf("%s", line);
+    if((fp = fopen(line,"r")) == NULL){
+        fprintf(stderr, "Нет такого файла\n");
+        return 1;
+    }
+    /* читаем строку за строкой */
+	p = line;
+    while(fgets (line, LINELENGTH, fp)){
+         while(sscanf(p, "  %d%n", &c, &n) == 1 ){
+			list_insert(test, c);
+             p +=n;
+        }
+    }
+ 
+	answer = list_LIS(test);
+	fclose(fp);
+
+	while ((c = getchar())!= '\n' && c != EOF);/* отсеять всякий мусор */
+	printf("Сохранить в файл? [y/n]:");
+	scanf("%c", &c);/* пропустить пробелы и получить ответ */
+	switch (c){
+    	case 'y': case 'Y': 
+    	printf("Записано в \"output.txt\"\n");
+    	fp = fopen("output.txt", "a");
+    	fprintf(fp, "------------------------\n");
+		while(answer->head != NULL){
+			fprintf(fp, "%d ", answer->head->number);
+			answer->head = answer->head->next;
+		}
+    	fprintf(fp,"\n");
+    	fclose(fp);
+    	break;
+	}
+
 	return 0;
 }
--- Курс 2/Введение в алгоритмы и основы технолог разработ пр/Лабораторная работа №3/list.c
+++ Курс 2/Введение в алгоритмы и основы технолог разработ пр/Лабораторная работа №3/list.c
@@ -1,8 +1,10 @@
 /* list.c */
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 #include "list.h"
 
+/* Сощдаем новый список */
 struct List *list_init(void)
 {
 	struct List* theList = malloc(sizeof(struct List));
@@ -13,6 +15,7 @@ struct List *list_init(void)
 	return theList;
 }
 
+/* Добавляем в конец списка */
 void list_insert(struct List *test, int number)
 {
 	struct Node *newNode = malloc(sizeof(struct Node));
@@ -25,4 +28,83 @@ void list_insert(struct List *test, int number)
 		test->tail->next = newNode;
 		test->tail = newNode;
 	}
+}
+
+/* Добавляем в начало списка */
+void list_push(struct List *test, int number)
+{
+	struct Node *newNode = malloc(sizeof(struct Node));
+	newNode->number = number;
+	newNode->next = NULL;	
+	if(test->head == NULL)
+		test->head = test->tail = newNode;
+	else{
+		newNode->next = test->head;
+		test->head = newNode;
+	}
+}
+
+/* Обрабатываем список  */
+struct List *list_LIS(struct List *test)
+{
+	int *prev = NULL;
+	int *len = NULL;
+	struct Node *tmp = test->head;
+	struct Node *first = NULL;
+	struct List *answer = NULL;
+	answer = list_init();
+	int i,j, n;
+	int pos, length;
+	length = i =  0;
+
+	/* пуст ли список? */
+
+	if(tmp == NULL)
+		return answer;	
+	while(tmp != NULL){
+
+		/* выделить память под очередные элементы массива*/
+		prev = realloc(prev, (++length) * sizeof(int));
+		len = realloc(len, (++length) * sizeof(int));
+		/* запомнить позицию головы */
+		first = test->head;
+		prev[i] = -1;
+		len[i] = j = 0;
+
+		/* находим количество элементов меньше определенного */
+		while(first != tmp){
+			if(first->number <= tmp->number &&
+			len[j] + 1 > len[i]){
+				len[i] = len[j] + 1; 
+				prev[i] = j;
+			}
+			first = first->next;
+			j++;
+		}
+		/* printf("%d --- %d --- %d\n", len[i], tmp->number, prev[i]);*/
+		tmp = tmp->next;
+		i++;
+	}
+	/* находим самую длинную последовательность чисел... */
+	n = length;
+	pos = 0;
+	length = len[0];
+	for(i = 0; i < n; i++)
+		if(len[i] > length){
+			pos = i;
+			length = len[i];
+		}
+
+	tmp = test->head;
+	i = 0;
+	/* ...и пихаем ее в список */
+	while(pos != -1){
+		for(i = 0; i < pos; i++)
+			tmp = tmp->next;
+		list_push(answer, tmp->number);
+		pos = prev[pos];
+		tmp = test->head;
+	}
+
+	return answer;
 }
